d06d31e8c5ce0bc75dda280c122f6f191d002019
Samy <Samy@Sakatiamy>
2014-04-11 20:45:06 +0200

    amelioration IA Factory

---
+++ src/intelligenceArtificielle/FactoryIA.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package intelligenceArtificielle;
+
+import bataille_navale.Parametre;
+
+/**
+ *
+ * @author Samy
+ */
+public class FactoryIA {
+    /////////////////////////// VARIABLES //////////////////////////////////
+
+    private static final FactoryIA INSTANCE = new FactoryIA();
+
+    //////////////////////// INITAILAISATION ///////////////////////////////
+    private FactoryIA() {
+
+    } // DAOFactory()
+
+    /**
+     * Point d'accès pour l'instance unique du singleton
+     *
+     * @return une instance du singleton
+     */
+    public static FactoryIA getInstance() {
+
+        return INSTANCE;
+
+    } // getInstance()
+
+    /////////////////////////// FONCTIONS //////////////////////////////////
+    public IntelligenceArtificielle getIntelligenceArtificielle(Parametre parametre, String difficulte) {
+        IntelligenceArtificielle intelligenceArtificielle = null;
+        switch (difficulte) {
+            case "Facile":
+                intelligenceArtificielle = new IntelligenceArtificielleFacile(parametre);
+                break;
+            case "Normal":
+                intelligenceArtificielle = new IntelligenceArtificielleMoyen(parametre);
+                break;
+            case "Difficile":
+                intelligenceArtificielle = new IntelligenceArtificielleDifficile(parametre);
+                break;
+        }
+        return intelligenceArtificielle;
+    }
+
+}
