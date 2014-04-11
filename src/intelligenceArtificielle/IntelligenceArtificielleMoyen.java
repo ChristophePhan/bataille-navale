8846d112ae9e4135ad172b402f24b27e5fbcf608
Samy <Samy@Sakatiamy>
2014-04-12 00:45:18 +0200

    IA Moyen Fonctionne (A tester un peu plus profondement)

--- src/intelligenceArtificielle/IntelligenceArtificielleMoyen.java
+++ src/intelligenceArtificielle/IntelligenceArtificielleMoyen.java
@@ -8,7 +8,6 @@ package intelligenceArtificielle;
 import bataille_navale.Case;
 import bataille_navale.Joueur;
 import bataille_navale.Parametre;
-import java.util.List;
 import java.util.Random;
 
 /**
@@ -18,8 +17,6 @@ import java.util.Random;
  */
 public class IntelligenceArtificielleMoyen extends IntelligenceArtificielle {
 
-    private List listeCaseATester;
-        
     public IntelligenceArtificielleMoyen(Parametre parametre) {
         super(parametre);
     }
@@ -28,7 +25,7 @@ public class IntelligenceArtificielleMoyen extends IntelligenceArtificielle {
     public Case getCaseForIA(Joueur joueurAdverse) {
         Random rand = new Random();
         Case caseTouchee = null;
-        if (listeCaseATester.isEmpty()) {
+        if (this.listeCaseATester.isEmpty()) {
             int x = rand.nextInt(this._parametre.getNbCaseX());
             int y = rand.nextInt(this._parametre.getNbCaseY());
             while (((Case) (joueurAdverse.getCases().get(x + y * this._parametre.getNbCaseX()))).isEtat()) {
@@ -36,16 +33,61 @@ public class IntelligenceArtificielleMoyen extends IntelligenceArtificielle {
                 y = rand.nextInt(this._parametre.getNbCaseY());
             }
             caseTouchee = (Case) (joueurAdverse.getCases().get(x + y * this._parametre.getNbCaseX()));
-            if (caseTouchee.isEtat()) {
-                listeCaseATester.add(caseTouchee);
+            if (caseTouchee.getClass().getSimpleName().equalsIgnoreCase("CaseBateau")) {
+                casesATester(joueurAdverse, caseTouchee);
             }
-        } /*else {
-            caseTouchee = (Case) listeCaseATester.get(0);
-            listeCaseATester.remove(caseTouchee);
-//            testerCase(joueurAdverse, caseTouchee);
-        }*/
-        System.out.println(listeCaseATester.toString());
+        } else {
+            caseTouchee = tester(joueurAdverse);
+        }
+        System.out.println("Case touchée " + caseTouchee.getClass().getSimpleName() + " : " + caseTouchee.getAbs() + ", " + caseTouchee.getOrd());
+        display();
         return caseTouchee;
+    }
+
+    private void casesATester(Joueur joueurAdverse, Case caseTouchee) {
+        Case case1 = caseTouchee.getAbs() != 0 ? joueurAdverse.getCases().get((caseTouchee.getAbs() - 1) + this._parametre.getNbCaseX() * caseTouchee.getOrd()) : caseTouchee;
+        Case case3 = caseTouchee.getOrd() != 0 ? joueurAdverse.getCases().get(caseTouchee.getAbs() + this._parametre.getNbCaseX() * (caseTouchee.getOrd() - 1)) : caseTouchee;
+
+        Case case2 = caseTouchee.getAbs() != (this._parametre.getNbCaseX() - 1) ? joueurAdverse.getCases().get((caseTouchee.getAbs() + 1) + this._parametre.getNbCaseX() * caseTouchee.getOrd()) : caseTouchee;
+        Case case4 = caseTouchee.getOrd() != (this._parametre.getNbCaseY() - 1) ? joueurAdverse.getCases().get(caseTouchee.getAbs() + this._parametre.getNbCaseX() * (caseTouchee.getOrd() + 1)) : caseTouchee;
+        
+//        if (!case1.isEtat() && !case1.equals(caseTouchee)) {
+//            this.listeCaseATester.add(case1);
+//        }
+//        if (!case2.isEtat() && !case2.equals(caseTouchee)) {
+//            this.listeCaseATester.add(case2);
+//        }
+//        if (!case3.isEtat() && !case3.equals(caseTouchee)) {
+//            this.listeCaseATester.add(case3);
+//        }
+//        if (!case4.isEtat() && !case4.equals(caseTouchee)) {
+//            this.listeCaseATester.add(case4);
+//        }
+        if (!case1.isEtat()) {
+            this.listeCaseATester.add(case1);
+        }
+        if (!case2.isEtat()) {
+            this.listeCaseATester.add(case2);
+        }
+        if (!case3.isEtat()) {
+            this.listeCaseATester.add(case3);
+        }
+        if (!case4.isEtat()) {
+            this.listeCaseATester.add(case4);
+        }
+    }
+
+    private Case tester(Joueur joueurAdverse) {
+        Random random = new Random();
+        int n = random.nextInt(listeCaseATester.size());
+        Case caseTestee = this.listeCaseATester.get(n);
+        if (caseTestee.getClass().getSimpleName().equalsIgnoreCase("CaseBateau")) {
+            casesATester(joueurAdverse, caseTestee);
+            this.listeCaseATester.remove(caseTestee);
+        } else {
+            this.listeCaseATester.remove(caseTestee);
+        }
+        return caseTestee;
     }
 
 }
